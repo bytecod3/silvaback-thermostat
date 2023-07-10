1e8019c4f77fe2f0a5043e45071d73a479d9eaed
#Eduh <emwiti658@gmail.com>
2023-07-10 22:43:18 +0300

    add adc config functions

---
+++ README.md
@@ -0,0 +1,1 @@
+# silvaback thermostat
---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include <driver/adc.h>
+
+/**
+ * Configure ADC_RTC
+ */
+void config_adc_rtc(){
+
+}
+
+
+void setup() {
+// write your initialization code here
+}
+
+void loop() {
+// write your code here
+}
